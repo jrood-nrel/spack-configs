125f3c8ee726c32ac9cfdfba84e2558a3b44c018
Jon Rood <jon.rood@nrel.gov>
2020-07-29 14:21:47 -0600

    Copy upstreams.yaml during install modules regardless of TYPE.

--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -91,9 +91,7 @@ if [ ! -d "${INSTALL_DIR}" ]; then
   cmd "cd ${THIS_REPO_DIR}/scripts && ./setup-spack.sh"
   cmd "cp ${THIS_REPO_DIR}/configs/${MACHINE}/${TYPE}/compilers.yaml ${SPACK_ROOT}/etc/spack/"
   cmd "cp ${THIS_REPO_DIR}/configs/${MACHINE}/${TYPE}/modules.yaml ${SPACK_ROOT}/etc/spack/"
-  if [ "${TYPE}" == 'utilities' ]; then
-    cmd "cp ${THIS_REPO_DIR}/configs/${MACHINE}/${TYPE}/upstreams.yaml ${SPACK_ROOT}/etc/spack/"
-  fi
+  cmd "cp ${THIS_REPO_DIR}/configs/${MACHINE}/${TYPE}/upstreams.yaml ${SPACK_ROOT}/etc/spack/ || true"
   cmd "mkdir -p ${SPACK_ROOT}/etc/spack/licenses/intel"
   cmd "cp ${HOME}/save/license.lic ${SPACK_ROOT}/etc/spack/licenses/intel/"
   cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
