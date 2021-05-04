ac8864aefda149343b9a46fa072a87df7d246a4f
Jon Rood <jon.rood@nrel.gov>
2021-05-04 16:04:03 -0600

    Use custom trilinos with ninja.

---
+++ custom/packages/trilinos/package.py
@@ -0,0 +1,11 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+from spack.pkg.builtin.openfast import Trilinos as bTrilinos
+
+class Trilinos(bTrilinos):
+    generator = 'Ninja'
+    depends_on('ninja', type='build')
